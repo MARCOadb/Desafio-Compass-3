1c87bf975e04aeb065790009ea36714f414d8e83
Marco Aurelio Batista <marco.batista.pb@compasso.com.br>
2023-06-13 03:41:25 -0300

    Criação das páginas Login e Signup (estáticas)

--- src/pages/LogIn/index.jsx
+++ src/pages/LogIn/index.jsx
@@ -1,8 +1,88 @@
+import './login.scss'
+import inicialImage from '../../assets/images/inicial_img.png'
+import user from '../../assets/icons/icon-user.svg'
+import lock from '../../assets/icons//icon-password.svg'
+import compassLogo from '../../assets/icons/Type=Colored negative.png'
+import { useState } from 'react'
 
 export default function LogIn() {
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    const [desUser, setDesUser] = useState(0)
+    const [desLock, setDesLock] = useState(0)
+
+    function handleFocusUser() {
+        setDesUser(-60)
+    }
+
+    function handleFocusLock() {
+        setDesLock(-60)
+    }
+
+    function handleBlurUser() {
+        if (name === '') {
+            setDesUser(0);
+        }
+    }
+
+    function handleBlurLock() {
+        if (password === '') {
+            setDesLock(0);
+        }
+    }
+
     return (
-        <div>
-            <h2>Login</h2>
+        <div className="content">
+            <div className="form-container">
+                <div className="form-content">
+                    <h1>Welcome,</h1>
+                    <span>To continue browsing safely, log in to the <br /> network.</span>
+
+                    <h2>Login</h2>
+                    <form>
+                        <div className='input-container'>
+                            <input
+                                type="text"
+                                // name='name'
+                                placeholder='user name'
+                                onChange={(e) => setName(e.target.value)}
+                                value={name}
+                                onFocus={handleFocusUser}
+                                onBlur={handleBlurUser}
+                            />
+                            <img
+                                src={user}
+                                alt="Icon"
+                                className='icon-user'
+                                style={{ transform: `translateX(${desUser}px)` }}
+                            />
+                        </div>
+                        <div className='input-container'>
+                            <input
+                                type="password"
+                                name='password'
+                                placeholder='password'
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                                onFocus={handleFocusLock}
+                                onBlur={handleBlurLock}
+                            />
+                            <img
+                                src={lock}
+                                alt="Icon"
+                                className='icon-lock'
+                                style={{ transform: `translateX(${desLock}px)` }}
+                            />
+                        </div>
+                        <button type='submit' className='btn-login'>Log in</button>
+                    </form>
+                </div>
+            </div>
+
+            <div className="img-container">
+                <img src={compassLogo} alt="Logo Compass" className='logo-img' />
+                <img src={inicialImage} alt="Computador" className='pc-img' />
+            </div>
         </div>
     )
 }
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,8 +1,66 @@
+import './signup.scss'
+import inicialImage from '../../assets/images/inicial_img.png'
+import compassLogo from '../../assets/icons/Type=Colored negative.png'
 
-export default function () {
+export default function SignUp() {
     return (
-        <div>
-            <h2>Signup</h2>
+        <div className="content">
+            <div className="form-container">
+                <div className="form-content">
+                    <h1>Welcome,</h1>
+                    <p>Please, register to continue</p>
+
+                    <form>
+                        <div className='input-container-2'>
+                            <label>first name</label>
+                            <input type="text" placeholder='Your first name' />
+                        </div>
+
+                        <div className='input-container-2'>
+                            <label>last name</label>
+                            <input type="text" placeholder='Your last name' />
+                        </div>
+
+                        <div className='input-container-2'>
+                            <label>birth date</label>
+                            <input type="date" placeholder='MM/DD/YYYY' />
+                        </div>
+
+                        <div className='input-container-2'>
+                            <label>Country</label>
+                            <input type="text" placeholder='Your Country' />
+                        </div>
+
+                        <div className='input-container-2'>
+                            <label>City</label>
+                            <input type="text" placeholder='Your City' />
+
+                        </div>
+
+                        <div className='input-container-2'>
+                            <label>e-mail</label>
+                            <input type="email" placeholder='A valid e-mail here' />
+                        </div>
+
+                        <div className='input-container-2'>
+                            <label>password</label>
+                            <input type="password" placeholder='Your password' />
+                        </div>
+
+                        <div className='input-container-2'>
+                            <label>password</label>
+                            <input type="password" placeholder='Confirm your password' />
+                        </div>
+
+                        <button type='submit' className='btn-register'>Register Now</button>
+                    </form>
+                </div>
+            </div>
+
+            <div className="img-container">
+                <img src={compassLogo} alt="Logo Compass" className='logo-img' />
+                <img src={inicialImage} alt="Computador" className='pc-img' />
+            </div>
         </div>
     )
 }
